3e6dc9059ce4927288880adde69d0f6d12292f34
eslam <elbadry693@gmail.com>
2024-03-09 17:53:14 +0200

    add config file

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.wowonder.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com
+// +------------------------------------------------------------------------+
+// | WoWonder - The Ultimate PHP Social Networking Platform
+// | Copyright (c) 2016 WoWonder. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "127.0.0.1";
+// MySQL Database User
+$sql_db_user = "root";
+// MySQL Database Password
+$sql_db_pass = "admin";
+// MySQL Database Name
+$sql_db_name = "neomers";
+// Site URL
+$site_url = "http://localhost/newomers"; // e.g (http://example.com)
+
+$auto_redirect = true;
+
+// Purchase code
+$purchase_code = "541113a8-c19a-4b54-aef5-344d0950272a"; // Your purchase code, don't give it to anyone.
+
+$siteEncryptKey = "26cc7aa09fdb3008e5e020499dc25b039476a73d"; // Your site encrypt key, don't give it to anyone.
+?>
